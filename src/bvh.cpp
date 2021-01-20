b795f98a51922a53fa74077f44b7b65d899813ab
Ilya Kavalerov <ilyakavalerov@gmail.com>
2021-01-20 17:41:44 -0500

    Updates for pytorch 1.7 compatibility

--- src/bvh.cpp
+++ src/bvh.cpp
@@ -22,8 +22,8 @@
 void bvh_cuda_forward(at::Tensor triangles, at::Tensor* collision_tensor_ptr,
         int max_collisions = 16);
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
-#define CHECK_CONTIGUOUS(x) AT_CHECK(x.is_contiguous(), #x " must be contiguous")
+#define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
 at::Tensor bvh_forward(at::Tensor triangles, int max_collisions = 16) {
